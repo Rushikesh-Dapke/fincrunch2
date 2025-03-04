f67c007f4f674b15a634a0ed3617738a6ab3ecbf
rushikeshdapke <rushikeshkdapke@example.com>
2025-03-04 12:08:13 +0530

    Initial commit

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+contact=environment.contactMail
+business=environment.businessMail
+career=environment.careerMail
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  contactMail:'contact@fincrunch.in',
+  businessMail:'businessfincrunch.in',
+  careerMail:'hr@fincrunch.in',
+  address:'114, B-wing, Sara Gokuldhaam, Airport Road, Chikhalthana,Aurangabad'
+};
